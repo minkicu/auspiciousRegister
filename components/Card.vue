63a6ae7f2464417da4d67fd87d9e6c6492e4c0b6
Thanatip chotipun <>
2023-02-15 00:31:12 +0700

    add main page ui

---
+++ components/Card.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="box  top-box">
+        <h2 class="title">{{ title }}</h2>
+        <p class="description">{{ description }}</p>
+        <p class="description" style="font-weight:700">คะแนนความโชคดีของคุณ : <span style="color:black">{{
+            luckyScore
+        }}</span> </p>
+        <p class="description" style="font-weight:700">สีนำโชค :
+            <span class="text-color" v-for="luckyColorItems in luckyColor"
+                :style="{ color: luckyColorItems.rgb_code }">{{
+                    luckyColorItems.name
+                }} </span>
+        </p>
+        <p class="description" style="font-weight:700">สีอับโชค :
+            <span class="text-color" v-for="unLuckyColorItems in unLuckyColor"
+                :style="{ color: unLuckyColorItems.rgb_code }">{{
+                    unLuckyColorItems.name
+                }} </span>
+        </p>
+        <img v-if="title" class="icon-card" :src="getIconUrl(title)" alt="" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        title: {
+            type: String,
+            default: 'Title',
+        },
+        description: {
+            type: String,
+            default: 'Description',
+        },
+        luckyScore: {
+            type: Number,
+            default: 0,
+        },
+        luckyColor: {
+            type: Array,
+            default() {
+                return [
+                    {
+                        name: '-',
+                        rgb_code: 'black',
+                    },
+                ]
+            }
+        },
+        unLuckyColor: {
+            type: Array,
+            default() {
+                return [
+                    {
+                        name: '-',
+                        rgb_code: 'black',
+                    },
+                ]
+            }
+        },
+    },
+    methods: {
+        getIconUrl(title) {
+            let iconUrl = 'https://assets.codepen.io/2301174/icon-supervisor.svg'
+            if (title === 'Financial') iconUrl = 'https://static.vecteezy.com/system/resources/previews/009/992/175/original/3d-finance-icon-png.png';
+            if (title === 'Work') iconUrl = 'https://icon-library.com/images/work-icon-png/work-icon-png-18.jpg';
+            if (title === 'Love') iconUrl = 'https://cdn-icons-png.flaticon.com/512/214/214309.png';
+            if (title === 'Luck') iconUrl = 'https://uxwing.com/wp-content/themes/uxwing/download/nature-and-environment/luck-lucky-icon.png';
+            if (title === 'Health') iconUrl = 'https://icons.veryicon.com/png/o/business/circular-multi-color-function-icon/health-health.png';
+            if (title === 'Negociate') iconUrl = 'https://static.thenounproject.com/png/3162144-200.png';
+            if (title === 'Support') iconUrl = 'https://cdn-icons-png.flaticon.com/512/190/190119.png';
+            return iconUrl
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.box {
+    position: relative;
+    border-radius: 5px;
+    box-shadow: 0px 30px 40px -20px hsl(229, 6%, 66%);
+    padding: 30px;
+    margin: 20px;
+    background-color: white;
+
+    .title {
+        font-size: 24px;
+        font-weight: 700;
+        color: hsl(229, 31%, 21%);
+    }
+
+    .description {
+        font-size: 16px;
+        font-weight: 400;
+        color: hsl(229, 8%, 60%);
+        margin-top: 10px;
+    }
+}
+
+.top-box {
+    border-top: 3px solid hsl(212, 86%, 64%);
+}
+
+.icon-card {
+    position: absolute;
+    top: -15px;
+    right: -20px;
+    width: 64px;
+    height: 64px;
+}
+
+.text-color {
+    text-shadow: 0.07em 0 black, 0 0.07em black, -0.07em 0 black, 0 -0.07em black;
+}
+</style>
---
+++ pages/main/index.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="homepage-wrapper">
+        <div class="navbar">
+            <div class="text-title text-center title">
+                Your lucky color
+            </div>
+        </div>
+
+        <div v-if="data">
+            <div v-for="items in data.today_data ">
+                <Card 
+                :title="items.topic" 
+                :description="items.description" 
+                :luckyScore="items.score"
+                :luckyColor="items.lucky_color"
+                :unLuckyColor="items.bad_color"
+                />
+            </div>
+        </div>
+
+
+
+
+    </div>
+  </template> 
+  
+  <script>
+  export default {
+    name: 'MainPage',
+    data() {
+        return {
+            data: null,
+        };
+    },
+    methods: {
+        getData() {
+            this.$axios
+                .get('https://astro.free.beeceptor.com/color/123456')
+                .then((res) => {
+                    console.log(res.data)
+                    this.data = res.data;
+                });
+        },
+    },
+    mounted() {
+        this.getData();
+    },
+  }
+  </script>
+  
+  
+  <style lang="scss" scoped>
+  .homepage-wrapper {
+    background: linear-gradient(180deg, #090837 0%, rgba(13, 12, 62, 0) 120%, rgba(13, 12, 62, 0) 100%);
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 15px;
+    align-items: center;
+    font-family: 'Inter';
+    overflow-x: hidden;
+    overflow-y: scroll;
+
+    .navbar {
+        border-bottom: 3px solid #e5caa8;
+        width: 110%;
+        overflow: visible;
+        padding-top: 30px;
+        padding-bottom: 20px;
+
+        .title {
+            font-family: "Inter";
+            color: #ffffff;
+            font-weight: 700;
+            font-size: 24px;
+        }
+    }
+
+  }
+
+  </style>
+  
